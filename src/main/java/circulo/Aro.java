22321346c820aca8ddc7f61472c88e918322857b
mario <mario@LAPTOP-EL78JEEL>
2022-03-05 13:16:14 +0100

    Actualizacion de comentarios

--- src/main/java/circulo/Aro.java
+++ src/main/java/circulo/Aro.java
@@ -1,94 +1,119 @@
 package circulo;
 
 /**
- * Probas de refactorización en NetBeans coa clase Aro
+ * Probas clase Aro
  * @author a21mariopm
  */
 public final class Aro {
-    public static final double MINIMO = 0.0;
-    public static final double LIMITERADIO = 0.0;
-    private int coordenadaX;
-    private int coordenadaY;
-    private double radio;
-    
-/**
- *  constructor vacio
- */
-    public Aro() {
-    }
- 
-    public Aro(int valorX, int valorY, double valorRadio) {
-        coordenadaX = valorX;
-        coordenadaY = valorY;
-        establecerRadio(valorRadio);
-    }
-
-    public void establecerX(int valorX) {
-        this.coordenadaX=valorX;
-    }
-
-    public int obterX() {
-        return getCoordenadaX();
-    }
-
-    public void establecerY(int valorY) {
-        this.coordenadaY=valorY;
-    }
-
-    public int obterY() {
-        return getCoordenadaY();
-    }
-
-    public void establecerRadio(double valorRadio) {
-
-        radio=(valorRadio < MINIMO ? MINIMO : valorRadio);
-    }
-
-    public double obterRadio() {
-        return getRadio();
-    }
-
-    public double obterDiametro() {
-        return getRadio() * 2;
-    }
-
-    public double obterCircunferencia() {
-        return Math.PI * obterDiametro();
-    }
-
-    public double obterSuperficie() {
-        return Math.PI * getRadio() * getRadio();
-    }
-
-    @Override
-    public String toString() {
-        return "Centro = [" + getCoordenadaX() + "," + getCoordenadaY() + "]; Radio = " + getRadio();
-    }
-
-    public void trasladarCentro(int trasladarX, int trasladarY){
-        coordenadaX=getCoordenadaX() + trasladarX;
-        coordenadaY=getCoordenadaY() + trasladarY;
-    }
 
     /**
-     * @return the coordenadaX
+     * @return a coordenadaX
      */
     public int getCoordenadaX() {
         return coordenadaX;
     }
 
     /**
-     * @return the coordenadaY
+     * @param valorX the valorX to set
+     */
+    public void setCoordenadaX(int valorX) {
+        this.coordenadaX = valorX;
+    }
+
+    /**
+     * @return a coordenadaY
      */
     public int getCoordenadaY() {
         return coordenadaY;
     }
 
     /**
-     * @return the radio
+     * @param valorY the valorY to set
+     */
+    public void setCoordenadaY(int valorY) {
+        this.coordenadaY = valorY;
+    }
+
+    /**
+     * @return o radio
      */
     public double getRadio() {
         return radio;
     }
+
+    /**
+     * 
+     * @param valorRadio the radio to set
+     */
+    public void setRadio(double valorRadio) {
+        this.radio = (valorRadio < MINIMO ? MINIMO : valorRadio);
+    }
+
+    private static final double LIMITERADIO = 0.0;
+
+    /**
+     * definición das variables
+     */
+    public static final double MINIMO = LIMITERADIO;
+
+    private int coordenadaX;
+    private int coordenadaY;
+    private double radio;
+
+    /**
+     * constructor vacío
+     */
+    public Aro() {
+    }
+
+    /**
+     * 
+     * @param valorX
+     * @param valorY
+     * @param valorRadio
+     */
+    public Aro(int valorX, int valorY, double valorRadio) {
+        coordenadaX = valorX;
+        coordenadaY = valorY;
+        setRadio(valorRadio);
+    }
+
+    /**
+     * Obten diametro do radio
+     * @return o radio multiplicado por 2
+     */
+    public double obterDiametro() {
+        return radio * 2;
+    }
+
+    /**
+     * Obtencircunferencia do aro
+     * @return Math.PI * obterDiametro()
+     */
+    public double obterCircunferencia() {
+        return Math.PI * obterDiametro();
+    }
+
+    /**
+     * Obten superficie do aro
+     * @return Math.PI * radio * radio
+     */
+    public double obterSuperficie() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Centro = [" + coordenadaX + "," + coordenadaY + "]; Radio = " + radio;
+    }
+
+    /**
+     *
+     * @param trasladarx
+     * @param trasladary
+     */
+    public void trasladarCentro(int trasladarx, int trasladary){
+        coordenadaX += trasladarx;
+        coordenadaY += trasladary;
+    }
 }
- 
